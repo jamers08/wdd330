2aa9f5b5c74212d0cfb3d5b024de2254f7ccd9ba
jamers08 <sto20046@byui.edu>
2022-10-22 10:03:11 -0400

    update

--- week5/js/main.js
+++ week5/js/main.js
@@ -1,7 +1,13 @@
-import Hikes from "./hiking-start.js";
+import Hikes from "js/hiking-start.js";
 
 const hikes = new Hikes("hikes");
 
 window.addEventListener("load", () => {
     hikes.showHikeList();
 })
+import Hikes from './hikes.js';
+//on load grab the array and insert it into the page
+const myHikes = new Hikes('hikes');
+window.addEventListener('load', () => {
+  myHikes.showHikeList();
+});
