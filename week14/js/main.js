f2307844a78c83447123c03e31b0e75df90e4cf8
jamers08 <sto20046@byui.edu>
2022-12-15 01:29:52 -0500

    update

--- week14/js/main.js
+++ week14/js/main.js
@@ -1,22 +1,110 @@
-const basePokemonURL = "https://pokeapi.co/api/v2/pokemon";
-const pokemonEvolution = "https://pokeapi.co/api/v2/evolution-chain";
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon";
+const drawCard = document.querySelector('#pokemonCard')
+const chooseType = document.querySelector('#types');
+const drawButton = document.querySelector('#drawBtn');
 
-//const resultsBox = 
-//const pokemonDetailsBox = 
+let pokeData = [];
+let shuffle = 0;
 
-async function fetchPokemon(url)
-{
-    response = await fetch(url);
-    if(response.ok){
-        const pokemon = await response.json();
-        console.log(pokemon);
-        return pokemon;
-    };
-}
+const normalURL = 'https://pokeapi.co/api/v2/type/1/';
+const fightingURL = 'https://pokeapi.co/api/v2/type/2/';
+const flyingURL = 'https://pokeapi.co/api/v2/type/3/';
+const poisonURL = 'https://pokeapi.co/api/v2/type/4/';
+const groundURL = 'https://pokeapi.co/api/v2/type/5/';
+const rockURL = 'https://pokeapi.co/api/v2/type/6/';
+const bugURL = 'https://pokeapi.co/api/v2/type/7/';
+const ghostURL = 'https://pokeapi.co/api/v2/type/8/';
+const steelURL = 'https://pokeapi.co/api/v2/type/9/';
+const fireURL = 'https://pokeapi.co/api/v2/type/10/';
+const waterURL = 'https://pokeapi.co/api/v2/type/11/';
+const grassURL = 'https://pokeapi.co/api/v2/type/12/';
+const electricURL = 'https://pokeapi.co/api/v2/type/13/';
+const psychicURL = 'https://pokeapi.co/api/v2/type/14/';
+const iceURL = 'https://pokeapi.co/api/v2/type/15/';
+const dragonURL = 'https://pokeapi.co/api/v2/type/16/';
+const darkURL = 'https://pokeapi.co/api/v2/type/17/';
+const fairyURL = 'https://pokeapi.co/api/v2/type/18/';
 
-function getPokemon(url) {
-    return fetchPokemon(url);
+async function fetchPokemon(url) {
+    await fetch(url)
+    .then(function(response) {
+        if(response.ok){
+            return response.json();
+        };
+    })
+    .then(function(getPokemon) {
+        getPokemon.results.forEach(function(pokemon){
+            displayPokemonCard(pokemon);
+        });
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+};
+
+async function getPokemonData(pokemon) {
+    await fetch(pokemon.url)
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        };
+    })
+    .then(function(results){
+        pokeData.push(results);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+};
+
+fetchPokemon(normalURL);
+
+function changeType() {
+    pokeData = [];
     
+    if(chooseType.value == 'normal') {
+        fetchPokemon(normalURL);
+    } else if (chooseType.value == 'fighting') {
+        fetchPokemon(fightingURL);
+    } else if (chooseType.value == 'flying') {
+        fetchPokemon(flyingURL);
+    } else if (chooseType.value == 'poison') {
+        fetchPokemon(poisonURL);
+    } else if (chooseType.value == 'ground') {
+        fetchPokemon(groundURL);
+    } else if (chooseType.value == 'rock') {
+        fetchPokemon(rockURL);
+    } else if (chooseType.value == 'bug') {
+        fetchPokemon(bugURL);
+    } else if (chooseType.value == 'ghost') {
+        fetchPokemon(ghostURL);
+    } else if (chooseType.value == 'steel') {
+        fetchPokemon(steelURL);
+    } else if (chooseType.value == 'fire') {
+        fetchPokemon(fireURL);
+    } else if (chooseType.value == 'water') {
+        fetchPokemon(waterURL);
+    } else if (chooseType.value == 'grass') {
+        fetchPokemon(grassURL);
+    } else if (chooseType.value == 'electric') {
+        fetchPokemon(electricURL);
+    } else if (chooseType.value == 'psychic') {
+        fetchPokemon(psychicURL);
+    } else if (chooseType.value == 'ice') {
+        fetchPokemon(iceURL);
+    } else if (chooseType.value == 'dragon') {
+        fetchPokemon(dragonURL);
+    } else if (chooseType.value == 'dark') {
+        fetchPokemon(darkURL);
+    } else if (chooseType.value == 'fairy') {
+        fetchPokemon(fairyURL);
+    } 
+};
+
+function displayPokemonCard() {
+    shuffle = Math.floor(Math.random() * pokeData.length);
+    let pic = document.creat
 }
 
-getPokemon();
+displayPokemonCard();
+
